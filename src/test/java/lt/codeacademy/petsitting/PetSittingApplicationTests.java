6e6270cca77fab04b27cea9337f6110c6cfe6af5
Ksistof <webselller92@gmail.com>
2022-12-13 08:44:05 +0200

    Initial commit

---
+++ src/test/java/lt/codeacademy/petsitting/PetSittingApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.petsitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSittingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
