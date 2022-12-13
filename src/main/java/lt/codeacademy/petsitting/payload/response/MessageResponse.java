2d6512202a1ede9a41307dca985bc59846bb0ef4
Ksistof <webselller92@gmail.com>
2022-12-13 20:41:33 +0200

    Implemented MessageResponse, LoginRequest, JwtResponse. Closes #27

---
+++ src/main/java/lt/codeacademy/petsitting/payload/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package lt.codeacademy.petsitting.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
