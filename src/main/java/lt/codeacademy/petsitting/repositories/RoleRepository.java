fa5bddf097d8b0fb88aa41b8b2dfd3a6b2c76de9
Ksistof <webselller92@gmail.com>
2022-12-13 12:47:40 +0200

    Added RoleRepository and findByName method. Closes #20

---
+++ src/main/java/lt/codeacademy/petsitting/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package lt.codeacademy.petsitting.repositories;
+
+import lt.codeacademy.petsitting.pojo.Role;
+import lt.codeacademy.petsitting.pojo.UserRoles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName( UserRoles role );
+}
