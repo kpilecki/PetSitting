b8bc3df1b259d979c14c6313adfce14f1bed5d11
Ksistof <webselller92@gmail.com>
2022-12-13 09:26:43 +0200

    Implemented Service Provider Repository. Closes #4

--- src/main/java/lt/codeacademy/petsitting/pojo/ServiceProvider.java
+++ src/main/java/lt/codeacademy/petsitting/pojo/ServiceProvider.java
@@ -1,9 +1,23 @@
 package lt.codeacademy.petsitting.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ServiceProvider extends Customer{
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
 
     public ServiceProvider(String username, String password, String firstName, String lastName) {
         super(username, password, firstName, lastName);
