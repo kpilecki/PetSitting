9a18e259ebf0ba6040116b82d2b102378347819b
Ksistof <webselller92@gmail.com>
2022-12-13 09:19:55 +0200

    Added missing annotation in User Repository. Closes #6

--- src/main/java/lt/codeacademy/petsitting/repositories/UserRepository.java
+++ src/main/java/lt/codeacademy/petsitting/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package lt.codeacademy.petsitting.repositories;
 
 import lt.codeacademy.petsitting.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
