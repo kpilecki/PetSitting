c7bb9ebfdbedc895b3160660177fdf78f5676667
Ksistof <webselller92@gmail.com>
2022-12-16 10:41:07 +0200

    Added Address to ServiceProvider. Closes #53

--- src/main/java/lt/codeacademy/petsitting/pojo/ServiceProvider.java
+++ src/main/java/lt/codeacademy/petsitting/pojo/ServiceProvider.java
@@ -4,11 +4,14 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @SuperBuilder
 @NoArgsConstructor
 @Entity
 public class ServiceProvider extends Customer{
 
+    @OneToOne
+    private Address publicAddress;
 
 }
