a6f24b2406713dd2f37619609d34ec77f1d6d9ce
Ksistof <webselller92@gmail.com>
2022-12-17 17:53:56 +0200

    Implemented CustomerController Address CRUD endpoints. Closes #56

--- src/main/java/lt/codeacademy/petsitting/controllers/CustomerController.java
+++ src/main/java/lt/codeacademy/petsitting/controllers/CustomerController.java
@@ -2,15 +2,17 @@ package lt.codeacademy.petsitting.controllers;
 
 import lt.codeacademy.petsitting.error.ApiError;
 import lt.codeacademy.petsitting.payload.response.MessageResponse;
+import lt.codeacademy.petsitting.pojo.Address;
 import lt.codeacademy.petsitting.pojo.Customer;
 import lt.codeacademy.petsitting.pojo.Role;
 import lt.codeacademy.petsitting.pojo.UserRoles;
+import lt.codeacademy.petsitting.services.AddressService;
 import lt.codeacademy.petsitting.services.CustomerService;
 import lt.codeacademy.petsitting.services.RoleService;
-import lt.codeacademy.petsitting.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
@@ -28,31 +30,32 @@ import java.util.Map;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/customers")
+@PreAuthorize( "hasRole('CUSTOMER')" )
 public class CustomerController {
 
     private final CustomerService customerService;
 
     private final RoleService roleService;
 
+    private final AddressService addressService;
+
     private final PasswordEncoder encoder;
 
     @Autowired
-    public CustomerController(CustomerService customerService, RoleService roleService, PasswordEncoder encoder) {
+    public CustomerController(CustomerService customerService, RoleService roleService, AddressService addressService, PasswordEncoder encoder) {
         this.customerService = customerService;
         this.roleService = roleService;
+        this.addressService = addressService;
         this.encoder = encoder;
     }
 
     @GetMapping( "/get" )
     public Customer loadCustomer(){
-        var auth = SecurityContextHolder.getContext().getAuthentication();
-        if( auth != null ){
-            return customerService.getByUsername( auth.getName() );
-        }
-        return null;
+        return getAuthenticatedCustomer();
     }
 
     @PostMapping( "/signup")
+    @PreAuthorize( "permitAll()")
     public ResponseEntity<?> registerCustomer(@Valid @RequestBody Customer customer ){
 
         Customer newCustomer = Customer.builder()
@@ -76,9 +79,48 @@ public class CustomerController {
         return ResponseEntity.ok( new MessageResponse( "Customer registered successfully!" ) );
     }
 
+    @PostMapping("/address")
+    public Address addOrUpdateAddress( @Valid @RequestBody Address address ){
+        Customer customer = getAuthenticatedCustomer();
+        assert customer != null;
+
+        if( customer.getAddress() == null ){
+            Address savedAddress = addressService.save( address );
+            customer.setAddress( savedAddress );
+        } else {
+            address.setId( customer.getAddress().getId() );
+            customer.setAddress( addressService.save( address ));
+        }
+        customerService.save( customer );
+        return customer.getAddress();
+    }
+
+    @DeleteMapping("/address/{id}")
+    public ResponseEntity<?> deleteAddress( @PathVariable( name = "id" ) Long id ){
+        Customer customer = getAuthenticatedCustomer();
+        assert customer != null;
+
+        if( customer.getAddress() == null || !customer.getAddress().getId().equals( id ) ){
+           return ResponseEntity.badRequest().body( "Error: Address does not exist" );
+        } else {
+            customer.setAddress( null );
+            customerService.save( customer );
+            addressService.deleteById( id );
+            return ResponseEntity.ok().body( "Address deleted successfully");
+        }
+    }
+
+    @GetMapping("/address")
+    public Address getAddress(){
+        Customer customer = getAuthenticatedCustomer();
+        assert customer != null;
+
+        return customer.getAddress();
+    }
+
     @ExceptionHandler( {MethodArgumentNotValidException.class} )
     @ResponseStatus( HttpStatus.BAD_REQUEST )
-    ApiError handleValidationException(MethodArgumentNotValidException exception, HttpServletRequest request ){
+    ApiError handleValidationException( MethodArgumentNotValidException exception, HttpServletRequest request ){
         ApiError apiError = new ApiError( 400, "Validation error", request.getServletPath() );
 
         BindingResult result = exception.getBindingResult();
@@ -91,6 +133,14 @@ public class CustomerController {
         apiError.setValidationErrors( validationErrors );
 
         return apiError;
+    }
+
+    private Customer getAuthenticatedCustomer(){
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        if( auth != null ){
+            return customerService.getByUsername( auth.getName() );
+        }
+        return null;
     }
 
 
