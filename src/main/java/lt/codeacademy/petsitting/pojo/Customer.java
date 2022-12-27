680d5d5123b39cab5ca0406335dd6a1165ceeef0
Ksistof <webselller92@gmail.com>
2022-12-27 09:49:36 +0200

    Added Pet to Customer. Closes #77

--- src/main/java/lt/codeacademy/petsitting/pojo/Customer.java
+++ src/main/java/lt/codeacademy/petsitting/pojo/Customer.java
@@ -1,13 +1,14 @@
 package lt.codeacademy.petsitting.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.cloud.storage.BlobId;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,7 +20,13 @@ public class Customer extends User{
     private String firstName;
     private String lastName;
     private BlobId profileImageId;
+
+    @JsonIgnore
     @OneToOne
     private Address address;
+
+    @JsonIgnore
+    @OneToMany( cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+    private List<Pet> pets;
 
 }
